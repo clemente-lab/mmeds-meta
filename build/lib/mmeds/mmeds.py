354572899ebe4ebcd654826fbc8823eeccc1b77c
DSWallach <d.s.t.wallach@gmail.com>
2017-11-26 09:21:41 -0500

    Got check_metadata working correctly

--- build/lib/mmeds/mmeds.py
+++ build/lib/mmeds/mmeds.py
@@ -1,14 +1,13 @@
-from subprocess import run
+from subprocess import run, PIPE
 
 
 def check_metadata(file_fp):
     """
     Execute QIIME to check the metadata file provided.
     """
-    sac = 'source activate qiime'
-    call_string = sac + 'validate_mapping_file.py ' + file_fp
-    try:
-        run(call_string, shell=True, check=True)
-        return 0
-    except ValueError:
-        return 1
+    sac = 'source activate qiime; '
+    call_string = sac + 'validate_mapping_file.py -m ' + file_fp
+    result = run(call_string, stdout=PIPE, shell=True, check=True)
+    # Return the result of the call
+    print(result.stdout)
+    return result.stdout
--- mmeds/mmeds.py
+++ mmeds/mmeds.py
@@ -1,14 +1,12 @@
-from subprocess import run
+from subprocess import run, PIPE
 
 
 def check_metadata(file_fp):
     """
     Execute QIIME to check the metadata file provided.
     """
-    sac = 'source activate qiime'
+    sac = 'source activate qiime; '
     call_string = sac + 'validate_mapping_file.py -m ' + file_fp
-    try:
-        run(call_string, shell=True, check=True)
-        return 0
-    except ValueError:
-        return 1
+    result = run(call_string, stdout=PIPE, shell=True, check=True)
+    # Return the result of the call
+    return result.stdout
