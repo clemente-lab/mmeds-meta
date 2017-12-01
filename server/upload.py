d3a84e8bcfbddd75aee9da67a84e6c1ee229b568
DSWallach <d.s.t.wallach@gmail.com>
2017-12-01 12:03:50 -0500

    Added HTTPS support

--- server/upload.py
+++ server/upload.py
@@ -105,5 +105,15 @@ class MMEDSserver(object):
                                  'attachment', os.path.basename(path))
 
 
+def secureheaders():
+    headers = cp.response.headers
+    headers['X-Frame-Options'] = 'DENY'
+    headers['X-XSS-Protection'] = '1; mode=block'
+    headers['Content-Security-Policy'] = 'default-src=self'
+    if cp.server.ssl_certificate and cp.server.ssl_private_key:
+        headers['Strict-Transport-Security'] = 'max-age=315360000'  # One Year
+
+
 if __name__ == '__main__':
+    cp.tools.secureheaders = cp.Tool('before_finalize', secureheaders, priority=60)
     cp.quickstart(MMEDSserver(), config=CONFIG)
