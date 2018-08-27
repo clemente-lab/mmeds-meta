8d3a825da86f744e0224e255edacea6b7f62c3fd
DSWallach <d.s.t.wallach@gmail.com>
2018-08-27 08:06:20 -0400

    setting server to timeout

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -1,4 +1,5 @@
 from locust import TaskSet, task, HttpLocust
+import mmeds.config as fig
 import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -8,10 +9,22 @@ class MyTasks(TaskSet):
 
     def on_start(self):
         self.client.verify = False
+        self.login()
+
+    def login(self):
+        self.client.post('/login',
+                         {
+                             'username': fig.TEST_USER,
+                             'password': fig.TEST_PASS
+                         })
 
     @task
     def read_root(self):
         self.client.get('/')
+
+    @task
+    def get_download(self):
+        self.client.get('/download_page?access_code={}'.format(fig.TEST_CODE))
 
 
 class MyUser(HttpLocust):
