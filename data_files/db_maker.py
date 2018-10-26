4a77e6f81e0b5998acdb8daabbf1c5537fcb3448
DSWallach <d.s.t.wallach@gmail.com>
2018-10-26 13:55:31 -0400

    should work

--- data_files/db_maker.py
+++ data_files/db_maker.py
@@ -1,14 +1,14 @@
-from mmeds.config import PROTECTED_TABLES, PUBLIC_TABLES
+from mmeds.config import PROTECTED_TABLES, PUBLIC_TABLES, SQL_DATABASE
 
-drop_sql = 'DROP VIEW IF EXISTS `mmeds`.`protected_{}`;\n'
-view_sql = 'CREATE\nSQL SECURITY DEFINER\nVIEW protected_{} AS\nSELECT cc.* FROM `mmeds`.`{}` cc WHERE owner_check(cc.user_id)\nWITH CHECK OPTION;\n\n'
-grant_sql = "GRANT SELECT ON TABLE `mmeds`.`protected_{}` TO 'mmeds_user'@'%';\n\n"
-public_sql = "GRANT SELECT ON TABLE `mmeds`.`{}` TO 'mmeds_user'@'%';\n\n"
+drop_sql = 'DROP VIEW IF EXISTS `{}`.`protected_{}`;\n'
+view_sql = 'CREATE\nSQL SECURITY DEFINER\nVIEW protected_{} AS\nSELECT cc.* FROM `{}`.`{}` cc WHERE owner_check(cc.user_id)\nWITH CHECK OPTION;\n\n'
+grant_sql = "GRANT SELECT ON TABLE `{}`.`protected_{}` TO 'mmeds_user'@'%';\n\n"
+public_sql = "GRANT SELECT ON TABLE `{}`.`{}` TO 'mmeds_user'@'%';\n\n"
 with open('/home/david/Work/mmeds-meta/sql/views.sql', 'w') as f:
     for table in PROTECTED_TABLES:
-        f.write(drop_sql.format(table))
-        f.write(view_sql.format(table, table))
-        f.write(grant_sql.format(table))
+        f.write(drop_sql.format(SQL_DATABASE, table))
+        f.write(view_sql.format(SQL_DATABASE, table, table))
+        f.write(grant_sql.format(SQL_DATABASE, table))
 
     for table in PUBLIC_TABLES:
-        f.write(public_sql.format(table))
+        f.write(public_sql.format(SQL_DATABASE, table))
--- sql/views.sql
+++ sql/views.sql
@@ -1,153 +1,153 @@
-DROP VIEW IF EXISTS `mmeds`.`protected_Lab`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_Lab`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_Lab AS
-SELECT cc.* FROM `mmeds`.`Lab` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `Lab`.`Lab` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_Lab` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_Lab` TO 'mmeds_user'@'%';
 
-DROP VIEW IF EXISTS `mmeds`.`protected_Study`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_Study`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_Study AS
-SELECT cc.* FROM `mmeds`.`Study` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `Study`.`Study` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_Study` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_Study` TO 'mmeds_user'@'%';
 
-DROP VIEW IF EXISTS `mmeds`.`protected_CollectionSite`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_CollectionSite`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_CollectionSite AS
-SELECT cc.* FROM `mmeds`.`CollectionSite` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `CollectionSite`.`CollectionSite` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_CollectionSite` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_CollectionSite` TO 'mmeds_user'@'%';
 
-DROP VIEW IF EXISTS `mmeds`.`protected_Experiment`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_Experiment`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_Experiment AS
-SELECT cc.* FROM `mmeds`.`Experiment` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `Experiment`.`Experiment` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_Experiment` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_Experiment` TO 'mmeds_user'@'%';
 
-DROP VIEW IF EXISTS `mmeds`.`protected_Subjects`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_Subjects`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_Subjects AS
-SELECT cc.* FROM `mmeds`.`Subjects` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `Subjects`.`Subjects` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_Subjects` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_Subjects` TO 'mmeds_user'@'%';
 
-DROP VIEW IF EXISTS `mmeds`.`protected_Illness`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_Illness`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_Illness AS
-SELECT cc.* FROM `mmeds`.`Illness` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `Illness`.`Illness` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_Illness` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_Illness` TO 'mmeds_user'@'%';
 
-DROP VIEW IF EXISTS `mmeds`.`protected_Intervention`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_Intervention`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_Intervention AS
-SELECT cc.* FROM `mmeds`.`Intervention` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `Intervention`.`Intervention` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_Intervention` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_Intervention` TO 'mmeds_user'@'%';
 
-DROP VIEW IF EXISTS `mmeds`.`protected_Specimen`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_Specimen`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_Specimen AS
-SELECT cc.* FROM `mmeds`.`Specimen` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `Specimen`.`Specimen` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_Specimen` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_Specimen` TO 'mmeds_user'@'%';
 
-DROP VIEW IF EXISTS `mmeds`.`protected_Aliquot`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_Aliquot`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_Aliquot AS
-SELECT cc.* FROM `mmeds`.`Aliquot` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `Aliquot`.`Aliquot` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_Aliquot` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_Aliquot` TO 'mmeds_user'@'%';
 
-DROP VIEW IF EXISTS `mmeds`.`protected_SampleProtocol`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_SampleProtocol`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_SampleProtocol AS
-SELECT cc.* FROM `mmeds`.`SampleProtocol` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `SampleProtocol`.`SampleProtocol` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_SampleProtocol` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_SampleProtocol` TO 'mmeds_user'@'%';
 
-DROP VIEW IF EXISTS `mmeds`.`protected_Sample`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_Sample`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_Sample AS
-SELECT cc.* FROM `mmeds`.`Sample` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `Sample`.`Sample` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_Sample` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_Sample` TO 'mmeds_user'@'%';
 
-DROP VIEW IF EXISTS `mmeds`.`protected_RawDataProtocol`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_RawDataProtocol`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_RawDataProtocol AS
-SELECT cc.* FROM `mmeds`.`RawDataProtocol` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `RawDataProtocol`.`RawDataProtocol` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_RawDataProtocol` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_RawDataProtocol` TO 'mmeds_user'@'%';
 
-DROP VIEW IF EXISTS `mmeds`.`protected_RawData`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_RawData`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_RawData AS
-SELECT cc.* FROM `mmeds`.`RawData` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `RawData`.`RawData` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_RawData` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_RawData` TO 'mmeds_user'@'%';
 
-DROP VIEW IF EXISTS `mmeds`.`protected_ResultsProtocol`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_ResultsProtocol`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_ResultsProtocol AS
-SELECT cc.* FROM `mmeds`.`ResultsProtocol` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `ResultsProtocol`.`ResultsProtocol` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_ResultsProtocol` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_ResultsProtocol` TO 'mmeds_user'@'%';
 
-DROP VIEW IF EXISTS `mmeds`.`protected_Results`;
+DROP VIEW IF EXISTS `mmeds_data1`.`protected_Results`;
 CREATE
 SQL SECURITY DEFINER
-VIEW protected_Results AS
-SELECT cc.* FROM `mmeds`.`Results` cc WHERE owner_check(cc.user_id)
+VIEW protected_mmeds_data1 AS
+SELECT cc.* FROM `Results`.`Results` cc WHERE owner_check(cc.user_id)
 WITH CHECK OPTION;
 
-GRANT SELECT ON TABLE `mmeds`.`protected_Results` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`protected_Results` TO 'mmeds_user'@'%';
 
-GRANT SELECT ON TABLE `mmeds`.`BodySite` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`RawDataProtocols` TO 'mmeds_user'@'%';
 
-GRANT SELECT ON TABLE `mmeds`.`Interventions` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`SampleProtocols` TO 'mmeds_user'@'%';
 
-GRANT SELECT ON TABLE `mmeds`.`Genotypes` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`Genotypes` TO 'mmeds_user'@'%';
 
-GRANT SELECT ON TABLE `mmeds`.`Illnesses` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`Interventions` TO 'mmeds_user'@'%';
 
-GRANT SELECT ON TABLE `mmeds`.`Ethnicity` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`Ethnicity` TO 'mmeds_user'@'%';
 
-GRANT SELECT ON TABLE `mmeds`.`RawDataProtocols` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`BodySite` TO 'mmeds_user'@'%';
 
-GRANT SELECT ON TABLE `mmeds`.`ResultsProtocols` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`Illnesses` TO 'mmeds_user'@'%';
 
-GRANT SELECT ON TABLE `mmeds`.`SampleProtocols` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`ResultsProtocols` TO 'mmeds_user'@'%';
 
-GRANT SELECT ON TABLE `mmeds`.`Type` TO 'mmeds_user'@'%';
+GRANT SELECT ON TABLE `mmeds_data1`.`Type` TO 'mmeds_user'@'%';
 
