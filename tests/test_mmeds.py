42faa6053310508a2aa962d5558f799091a96406
DSWallach <d.s.t.wallach@gmail.com>
2018-10-30 13:44:33 -0400

    Removed easyimap

--- tests/test_mmeds.py
+++ tests/test_mmeds.py
@@ -1,9 +1,5 @@
 from mmeds import mmeds
-from time import sleep
 import mmeds.config as fig
-import easyimap
-import email
-import datetime
 import hashlib as hl
 import os
 import pandas as pd
@@ -17,25 +13,6 @@ def test_is_numeric():
     assert mmeds.is_numeric('r5') is False
     assert mmeds.is_numeric('5r') is False
     assert mmeds.is_numeric('2016-12-01') is False
-
-
-# Not in current use
-def get_email():
-    mmeds.send_email(fig.TEST_EMAIL, fig.TEST_USER, code=fig.TEST_CODE)
-    imapper = easyimap.connect('imap.gmail.com', fig.TEST_EMAIL, fig.TEST_EMAIL_PASS)
-    sleep(10)
-    for mail in imapper.unseen(limit=1):
-        # Assert the email was sent from the correct address
-        assert mail.from_addr == fig.MMEDS_EMAIL
-
-        # Get the timestamp from the email
-        parsed = email.utils.parsedate_tz(mail.date)
-        email_datetime = datetime.datetime(*parsed[:-3])
-        delta = datetime.datetime.now() - email_datetime
-        # parsed[-1] contains the timezone offset
-        assert delta.total_seconds() + parsed[-1] < 600
-
-    imapper.quit()
 
 
 def test_create_local_copy():
