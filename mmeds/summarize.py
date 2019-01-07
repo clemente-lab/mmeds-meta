fc1ea72cddb2bb1784f2c49e23669aff530167c3
dswallach <d.s.t.wallach@gmail.com>
2019-01-07 12:32:37 -0500

    Added documentation to mmeds/summarize.py

--- mmeds/summarize.py
+++ mmeds/summarize.py
@@ -10,25 +10,27 @@ from mmeds.config import STORAGE_DIR
 from mmeds.mmeds import log
 
 
-def summarize_qiime1(metadata=['Ethnicity', 'Nationality'], files={}, execute=False, name='analysis', run_path='/home/david/Work/data-mmeds/summary'):
+def summarize_qiime1(metadata=['Ethnicity', 'Nationality'],
+                     files={},
+                     execute=False,
+                     name='analysis',
+                     run_path='/home/david/Work/data-mmeds/summary'):
     """
     Create the summary PDF for qiime1 analysis
+    ------------------------------------------
+    :metadata: A list of strings. Includes the names of metadata columns to include in the analysis.
+    :files: A dictionary of locations for the files to use when creating plots.
+    :execute: A boolean. If True execute the notebook when exporting to PDF, otherwise don't.
+    :name: A string. The name of the notebook and PDF document.
+    :run_path: A file path. The path to the directory containing all the summary files.
     """
-    log('Start summary notebook')
-    original_path = Path.cwd()
-    os.chdir(run_path)
-    # Load the code templates
-    with open(STORAGE_DIR / 'summary_code.txt') as f:
-        data = f.read().split('\n=====\n')
 
-    # Dict for storing all the different code templates
-    source = {}
-    for code in data:
-        parts = code.split('<source>\n')
-        source[parts[0]] = parts[1]
-
-    def taxa_plots(path, data_file):
-        """ Create plots for taxa summary files. """
+    def taxa_plots(data_file):
+        """
+        Create plots for taxa summary files.
+        ------------------------------------
+        :data_file: The location of the file to create the plotting code for.
+        """
         cells = []
         for column in metadata:
             filename = '{}-{}.png'.format(data_file.split('.')[0], column)
@@ -38,8 +40,12 @@ def summarize_qiime1(metadata=['Ethnicity', 'Nationality'], files={}, execute=Fa
             cells.append(v4.new_code_cell(source='Image("{plot}")'.format(plot=filename)))
         return cells
 
-    def alpha_plots(path, data_file):
-        """ Create plots for alpha diversity files. """
+    def alpha_plots(data_file):
+        """
+        Create plots for alpha diversity files.
+        ---------------------------------------
+        :data_file: The location of the file to create the plotting code for.
+        """
         filename = data_file.split('.')[0] + '.png'
         cells = []
         cells.append(v4.new_markdown_cell(source='## View {f}'.format(f=data_file)))
@@ -48,8 +54,12 @@ def summarize_qiime1(metadata=['Ethnicity', 'Nationality'], files={}, execute=Fa
         cells.append(v4.new_code_cell(source='Image("{plot}")'.format(plot=filename)))
         return cells
 
-    def beta_plots(path, data_file):
-        """ Create plots for alpha diversity files. """
+    def beta_plots(data_file):
+        """
+        Create plots for alpha diversity files.
+        ---------------------------------------
+        :data_file: The location of the file to create the plotting code for.
+        """
         cells = []
         for column in metadata:
             plot = '{}-{}.png'.format(data_file.split('.')[0], column)
@@ -64,7 +74,14 @@ def summarize_qiime1(metadata=['Ethnicity', 'Nationality'], files={}, execute=Fa
         return cells
 
     def summarize(path, files, execute, no_files=False):
-        """ Create the python notebook containing the summary of analysis results. """
+        """
+        Create the python notebook containing the summary of analysis results.
+        ----------------------------------------------------------------------
+        :path: A file path. The path to the directory containing the files to plot
+        :files: A dictionary of locations for the files to use when creating plots.
+        :execute: A boolean. If True execute the notebook when exporting to PDF, otherwise don't.
+        :no_files: A boolean. If True use a local file_index.tsv file to populate the files dict.
+        """
         # Get the files to summarize from the index
         if no_files:
             files = defaultdict(list)
@@ -74,43 +91,51 @@ def summarize_qiime1(metadata=['Ethnicity', 'Nationality'], files={}, execute=Fa
                 parts = line.strip('\n').split('\t')
                 files[parts[0]].append(parts[1])
 
-        # Create the list to store all cells
+        # Used to store the notebook cells
         cells = []
 
         with open(path / 'biom_table_summary.txt') as f:
             output = f.read().replace('\n', '  \n').replace('\r', '  \r')
 
-        # Add all the cells containing the different files
+        # Add cells for setting up the R and Python environments
         cells.append(v4.new_markdown_cell(source='# Notebook Setup'))
         cells.append(v4.new_code_cell(source=source['py_setup']))
         cells.append(v4.new_code_cell(source=source['r_setup']))
+
+        # Add the cells for the OTU summary
         cells.append(v4.new_markdown_cell(source='# OTU Summary'))
         cells.append(v4.new_markdown_cell(source=output))
         cells.append(v4.new_markdown_cell(source='To view the full otu table, execute the code cell below'))
         cells.append(v4.new_code_cell(source=source['otu_py']))
+
+        # Add the cells for the Taxa summaries
         cells.append(v4.new_markdown_cell(source='# Taxa Summary'))
         for data_file in files['taxa']:
-            cells += taxa_plots(path, data_file)
+            cells += taxa_plots(data_file)
         cells.append(v4.new_markdown_cell(source='# Diversity Plot Legend'))
         cells.append(v4.new_code_cell(source=source['legend_py'].format()))
         cells.append(v4.new_code_cell(source=source['legend_r'].format(plot='legend.png')))
         cells.append(v4.new_code_cell(source='Image("{plot}")'.format(plot='legend.png')))
+
+        # Add the cells for Alpha Diversity
         cells.append(v4.new_markdown_cell(source='# Alpha Diversity Summary'))
         for data_file in files['alpha']:
-            cells += alpha_plots(path, data_file)
+            cells += alpha_plots(data_file)
+
+        # Add the cells for Beta Diversity
         cells.append(v4.new_markdown_cell(source='# Beta Diversity Summary'))
         for data_file in sorted(files['beta']):
             if 'dm' in data_file:
                 cells.append(v4.new_markdown_cell(source="## View {file1}".format(file1=data_file)))
                 cells.append(v4.new_code_cell(source="df = read_csv('{file1}', sep='\t')".format(file1=data_file)))
             else:
-                cells += beta_plots(path, data_file)
+                cells += beta_plots(data_file)
 
         # Hide the code in all cells
         for cell in cells:
-            cell.metadata['hide_code'] = True
             cell.metadata['hide_input'] = True
-        nn = nbf.v4.new_notebook(cells=cells)
+
+        # Create the notebook and
         meta = {
             'metadata': {
                 'authors': [{'name': 'David Wallach', 'email': 'david.wallach@mssm.edu'}],
@@ -119,12 +144,16 @@ def summarize_qiime1(metadata=['Ethnicity', 'Nationality'], files={}, execute=Fa
                 'path': '{path}/'.format(path=path)
             }
         }
-        nn.update(meta)
+        nn = nbf.v4.new_notebook(cells=cells, metadata=meta)
         return nn
 
     def write_notebook(nn):
+        """
+        Write the notebook and export it to a PDF.
+        ==========================================
+        :nn: A python notebook object.
+        """
         nbf.write(nn, str(path / '{}.ipynb'.format(name)))
-
         cmd = 'jupyter nbconvert --template=nbextensions --to=latex {}.ipynb'.format(name)
         if execute:
             cmd += ' --execute'
@@ -138,8 +167,23 @@ def summarize_qiime1(metadata=['Ethnicity', 'Nationality'], files={}, execute=Fa
         cmd = 'pdflatex {name}.tex {name}.pdf'.format(name=name)
         run(cmd, shell=True, check=True)
 
+    log('Start summary notebook')
+    original_path = Path.cwd()
+    os.chdir(run_path)
+    # Load the code templates
+    with open(STORAGE_DIR / 'summary_code.txt') as f:
+        data = f.read().split('\n=====\n')
+
+    # Dict for storing all the different code templates
+    source = {}
+    for code in data:
+        parts = code.split('<source>\n')
+        source[parts[0]] = parts[1]
+
     path = Path(run_path)
 
     nn = summarize(path, files, execute)
     write_notebook(nn)
+
+    # Switch back to the original directory
     os.chdir(original_path)
