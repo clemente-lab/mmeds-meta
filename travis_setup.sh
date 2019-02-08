32cffc49310674179108501c288ed0f007ad4b77
dswallach <d.s.t.wallach@gmail.com>
2019-02-08 08:16:19 -0500

    try tagain

--- travis_setup.sh
+++ travis_setup.sh
@@ -27,14 +27,14 @@ else
 fi
 if [ ! -d "~/.modules/qiime1" ]; then
     echo "Create qiime1 environment"
-    conda create python=2.7 qiime matplotlib=1.4.3 mock nose -c bioconda --yes --quiet --copy -p ~/.modules/qiime1 &>> ~/install_log.txt;
+    conda create python=2.7 qiime matplotlib=1.4.3 mock nose -c bioconda --yes --quiet --copy -p ~/.modules/qiime1;
 else
     echo "Already loaded";
 fi
 if [ ! -d "~/.modules/qiime2" ]; then
     echo "Create qiime2 environment"
     wget https://data.qiime2.org/distro/core/qiime2-2019.1-py36-linux-conda.yml -O ~/qiime2.yml --quiet;
-    conda env create --file ~/qiime2.yml --quiet -p ~/.modules/qiime2 &>> ~/install_log.txt;
+    conda env create --file ~/qiime2.yml --quiet -p ~/.modules/qiime2;
 else
     echo "Already loaded";
 fi
