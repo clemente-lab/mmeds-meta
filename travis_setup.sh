9425ba47b3f21b84fbe7b0638b5ae31d46c86468
dswallach <d.s.t.wallach@gmail.com>
2019-02-11 12:55:37 -0500

    fix

--- travis_setup.sh
+++ travis_setup.sh
@@ -15,6 +15,21 @@ if [ ! -d ~/.modules/modulefiles ]; then
     echo "make .modules/modules";
     mkdir ~/.modules/modulefiles
 fi
+if [ ! -d ~/.modules/qiime1 ]; then
+    echo "Create qiime1 environment"
+    ls -a ~/.modules;
+    conda create python=2.7 qiime matplotlib=1.4.3 mock nose -c bioconda --yes --quiet --copy -p ~/.modules/qiime1;
+fi
+if [ ! -d ~/.modules/qiime2 ]; then
+    echo "Create qiime2 environment"
+    wget https://data.qiime2.org/distro/core/qiime2-2019.1-py36-linux-conda.yml -O ~/qiime2.yml --quiet;
+    ls -a ~/.modules;
+    conda env create --file ~/qiime2.yml --quiet -p ~/.modules/qiime2;
+fi
+
+# TEMPORARY
+rm -rf ~/.modules/modulefiles/*;
+
 if [ ! -f ~/.modules/modulefiles/qiime1 ]; then
     echo "Create qiime1 module";
     printf "#%Module1.0\n## qiime1 modulefile\nset curMod [module-info name]\nmodule-info name qiime1\nmodule-info version 1.9.1\nprepend-path PATH ~/.modules/qiime1/bin" > ~/.modules/modulefiles/qiime1
@@ -26,17 +41,6 @@ fi
 if [ ! -f ~/.modules/modulefiles/mmeds-stable ]; then
     echo "Create mmeds-stable module";
     printf "#%Module1.0\n## mmeds-stable modulefile\nset curMod [module-info name]\nmodule-info name mmeds-stable\nmodule-info version 1.5.1\nprepend-path PATH ~/miniconda2/envs/mmeds-stable/bin" > ~/.modules/modulefiles/mmeds-stable
-fi
-if [ ! -d ~/.modules/qiime1 ]; then
-    echo "Create qiime1 environment"
-    ls -a ~/.modules;
-    conda create python=2.7 qiime matplotlib=1.4.3 mock nose -c bioconda --yes --quiet --copy -p ~/.modules/qiime1;
-fi
-if [ ! -d ~/.modules/qiime2 ]; then
-    echo "Create qiime2 environment"
-    wget https://data.qiime2.org/distro/core/qiime2-2019.1-py36-linux-conda.yml -O ~/qiime2.yml --quiet;
-    ls -a ~/.modules;
-    conda env create --file ~/qiime2.yml --quiet -p ~/.modules/qiime2;
 fi
 
 if [ ! -d ~/.local/modules-4.2.1.tar.gz ]; then
