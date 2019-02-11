6071a4c4ac53b34221113e0aaae4ffeae03dc374
dswallach <d.s.t.wallach@gmail.com>
2019-02-11 12:06:24 -0500

    agaaagagagag

--- mmeds/qiime2.py
+++ mmeds/qiime2.py
@@ -14,10 +14,9 @@ class Qiime2(Tool):
     def __init__(self, owner, access_code, atype, config, testing):
         super().__init__(owner, access_code, atype, config, testing)
         if testing:
-            #  self.jobtext.append('modulecmd bash use ~/.modules/modulefiles; modulecmd bash load qiime2;')
-            self.jobtext.append('module use ~/.modules/modulefiles; module load qiime2;')
+            self.jobtext.append('module load qiime2;')
         else:
-            self.jobtext.append('module use $MMEDS/.modules/modulefiles; module use /hpc/packages/minerva-common/modulefiles; module load qiime2/2018.4;')
+            self.jobtext.append('module load qiime2/2018.4;')
 
     # ======================= #
     # # # Qiime2 Commands # # #
