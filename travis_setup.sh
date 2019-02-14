3c486f60a605c8122a4579d4ed4868e7217439c3
dswallach <d.s.t.wallach@gmail.com>
2019-02-14 09:41:45 -0500

    removed qiime2 setup from travis

--- travis_setup.sh
+++ travis_setup.sh
@@ -23,11 +23,11 @@ if [ ! -d ~/.modules/qiime1 ]; then
     echo "Create qiime1 environment"
     conda create python=2.7 qiime matplotlib=1.4.3 mock nose -c bioconda --yes --quiet --copy -p ~/.modules/qiime1 &>/dev/null;
 fi
-if [ ! -d ~/.modules/qiime2 ]; then
-    echo "Create qiime2 environment"
-    wget https://data.qiime2.org/distro/core/qiime2-2019.1-py36-linux-conda.yml -O ~/qiime2.yml --quiet;
-    conda env create --file ~/qiime2.yml --quiet -p ~/.modules/qiime2 &>/dev/null;
-fi
+#if [ ! -d ~/.modules/qiime2 ]; then
+#    echo "Create qiime2 environment"
+#    wget https://data.qiime2.org/distro/core/qiime2-2019.1-py36-linux-conda.yml -O ~/qiime2.yml --quiet;
+#    conda env create --file ~/qiime2.yml --quiet -p ~/.modules/qiime2 &>/dev/null;
+#fi
 if [ ! -d ~/.modules/mmeds-stable ]; then
     echo "Create mmeds environment"
     conda create --file spec-file.txt -p ~/.modules/mmeds-stable --quiet &>/dev/null;
@@ -39,10 +39,10 @@ if [ ! -f ~/.modules/modulefiles/qiime1 ]; then
     echo "Create qiime1 module";
     printf "#%%Module1.0\n## qiime1 modulefile\nset curMod [module-info name]\nmodule-info name qiime1\nmodule-info version 1.9.1\nprepend-path PATH ~/.modules/qiime1/bin" > ~/.modules/modulefiles/qiime1
 fi
-if [ ! -f ~/.modules/modulefiles/qiime2 ]; then
-    echo "Create qiime2 module";
-    printf "#%%Module1.0\n## qiime2 modulefile\nset curMod [module-info name]\nmodule-info name qiime2\nmodule-info version 2019.1\nprepend-path PATH ~/.modules/qiime2/bin" > ~/.modules/modulefiles/qiime2
-fi
+#if [ ! -f ~/.modules/modulefiles/qiime2 ]; then
+#    echo "Create qiime2 module";
+#    printf "#%%Module1.0\n## qiime2 modulefile\nset curMod [module-info name]\nmodule-info name qiime2\nmodule-info version 2019.1\nprepend-path PATH ~/.modules/qiime2/bin" > ~/.modules/modulefiles/qiime2
+#fi
 if [ ! -f ~/.modules/modulefiles/mmeds-stable ]; then
     echo "Create mmeds-stable module";
     printf "#%%Module1.0\n## mmeds-stable modulefile\nset curMod [module-info name]\nmodule-info name mmeds-stable\nmodule-info version 1.5.1\nprepend-path PATH ~/.modules/mmeds-stable/bin" > ~/.modules/modulefiles/mmeds-stable
