4e4f41568474c3a65b83cd8030ac5ed09ed7728c
dswallach <d.s.t.wallach@gmail.com>
2019-07-01 09:42:22 -0400

    better setup for Jenkins

--- travis_setup.sh
+++ travis_setup.sh
@@ -10,47 +10,35 @@ chmod +x ~/miniconda.sh;
 export PATH=~/miniconda2/bin:$PATH;
 conda update --yes conda;
 
-if [ ! -d ~/.modules ]; then
-    echo 'Make .modules';
-    mkdir ~/.modules;
-fi
-
-if [ ! -d ~/.modules/modulefiles ]; then
-    echo 'Make .modules/modulefiles';
-    mkdir ~/.modules/modulefiles;
-fi
+echo 'Make .modules';
+mkdir ~/.modules;
+echo 'Make .modules/modulefiles';
+mkdir ~/.modules/modulefiles;
 
 # Create the necessary conda environments
-if [ ! -d ~/.modules/mmeds-stable ]; then
-    echo "Create mmeds-stable environment";
-    conda create --file spec-file.txt -p ~/.modules/mmeds-stable --yes --quiet --copy;
-    ln -sf ~/.modules/mmeds-stable ~/miniconda2/envs/mmeds-stable;
-    source activate mmeds-stable;
-    echo "Install R libraries";
-    Rscript setup.R;
-    source deactivate;
-fi
-if [ ! -d ~/.modules/qiime1 ]; then
-    echo "Create qiime1 environment";
-    conda create python=2.7 qiime matplotlib=1.4.3 mock nose -c bioconda --yes --quiet --copy -p ~/.modules/qiime1;
-fi
-if [ ! -d ~/.modules/qiime2 ]; then
-    echo "Create qiime2 environment"
-    wget https://data.qiime2.org/distro/core/qiime2-2019.1-py36-linux-conda.yml -O ~/qiime2.yml --quiet;
-    conda env create --file ~/qiime2.yml --quiet -p ~/.modules/qiime2;
-fi
+echo "Create mmeds-stable environment";
+conda create --file spec-file.txt -p ~/.modules/mmeds-stable --yes --quiet --copy;
+ln -sf ~/.modules/mmeds-stable ~/miniconda2/envs/mmeds-stable;
+source activate mmeds-stable;
+echo "Install R libraries";
+Rscript setup.R;
+source deactivate;
+
+echo "Create qiime1 environment";
+conda create python=2.7 qiime matplotlib=1.4.3 mock nose -c bioconda --yes --quiet --copy -p ~/.modules/qiime1;
+echo "Create qiime2 environment"
+wget https://data.qiime2.org/distro/core/qiime2-2019.1-py36-linux-conda.yml -O ~/qiime2.yml --quiet;
+conda env create --file ~/qiime2.yml --quiet -p ~/.modules/qiime2;
 
 # Configure and install environment modules
-if [ ! -d ~/.local ]; then
-    echo "Install environment-modules";
-    wget https://sourceforge.net/projects/modules/files/Modules/modules-4.2.1/modules-4.2.1.tar.gz -O modules-4.2.1.tar.gz &>/dev/null;
-    tar -zxf modules-4.2.1.tar.gz;
-    cd modules-4.2.1;
-    ./configure --prefix="${HOME}/.local" --modulefilesdir="${HOME}/.modules/modulefiles" #&>/dev/null;
-    make;
-    sudo make install;
-    cd $REPO_DIR;
-fi
+echo "Install environment-modules";
+wget https://sourceforge.net/projects/modules/files/Modules/modules-4.2.1/modules-4.2.1.tar.gz -O modules-4.2.1.tar.gz &>/dev/null;
+tar -zxf modules-4.2.1.tar.gz;
+cd modules-4.2.1;
+./configure --prefix="${HOME}/.local" --modulefilesdir="${HOME}/.modules/modulefiles" #&>/dev/null;
+make;
+sudo make install;
+cd $REPO_DIR;
 
 # Make sure module will work
 export PATH="$HOME/.local/bin:$PATH"
