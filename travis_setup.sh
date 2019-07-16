58dca4ae97edede6effdeb9fc31fef2b522b0cee
dswallach <d.s.t.wallach@gmail.com>
2019-07-16 08:42:12 -0400

    Try old travis settings

--- travis_setup.sh
+++ travis_setup.sh
@@ -3,62 +3,69 @@ echo "In $(pwd)"
 export REPO_DIR=$(pwd)
 echo "Running setup from ${REPO_DIR}"
 
-# Anaconda setup
-wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O ~/miniconda.sh --quiet;
-chmod +x ~/miniconda.sh;
-~/miniconda.sh -b;
-export PATH=~/miniconda2/bin:$PATH;
-conda update --yes conda;
+if [ ! -d ~/.modules ]; then
+    echo 'Make .modules';
+    mkdir ~/.modules;
+    mkdir ~/.modules/modulefiles;
+    sudo chown -R travis:travis ~/.modules
+fi
 
-echo 'Make .modules';
-mkdir ~/.modules;
-echo 'Make .modules/modulefiles';
-mkdir ~/.modules/modulefiles;
+# Create module files (and install R packages)
+if [ ! -f ~/.modules/modulefiles/qiime1 ]; then
+    echo "Create qiime1 module";
+    printf "#%%Module1.0\n## qiime1 modulefile\nset curMod [module-info name]\nmodule-info name qiime1\nmodule-info version 1.9.1\nprepend-path PATH ~/.modules/qiime1/bin" > ~/.modules/modulefiles/qiime1
+fi
+#if [ ! -f ~/.modules/modulefiles/qiime2 ]; then
+#    echo "Create qiime2 module";
+#    printf "#%%Module1.0\n## qiime2 modulefile\nset curMod [module-info name]\nmodule-info name qiime2\nmodule-info version 2019.1\nprepend-path PATH ~/.modules/qiime2/bin" > ~/.modules/modulefiles/qiime2
+#fi
+if [ ! -f ~/.modules/modulefiles/mmeds-stable ]; then
+    echo "Create mmeds-stable module";
+    printf "#%%Module1.0\n## mmeds-stable modulefile\nset curMod [module-info name]\nmodule-info name mmeds-stable\nmodule-info version 1.5.1\nprepend-path PATH ~/.modules/mmeds-stable/bin" > ~/.modules/modulefiles/mmeds-stable
+fi
 
 # Create the necessary conda environments
-echo "Create mmeds-stable environment";
-conda create --file ~/mmeds-meta/spec-file.txt -p ~/.modules/mmeds-stable --yes --quiet --copy;
-ln -sf ~/.modules/mmeds-stable ~/miniconda2/envs/mmeds-stable;
-source activate mmeds-stable;
-echo "Install R libraries";
-Rscript ~/mmeds-meta/setup.R;
-source deactivate;
-
-echo "Create qiime1 environment";
-conda create python=2.7 qiime matplotlib=1.4.3 mock nose -c bioconda --yes --quiet --copy -p ~/.modules/qiime1;
-echo "Create qiime2 environment"
-wget https://data.qiime2.org/distro/core/qiime2-2019.1-py36-linux-conda.yml -O ~/qiime2.yml --quiet;
-conda env create --file ~/qiime2.yml --quiet -p ~/.modules/qiime2;
+if [ ! -d ~/.modules/mmeds-stable ]; then
+    echo "Create mmeds-stable environment";
+    conda create --file spec-file.txt -p ~/.modules/mmeds-stable --yes --quiet --copy &>/dev/null;
+    ln -sf ~/.modules/mmeds-stable ~/miniconda2/envs/mmeds-stable;
+    source activate mmeds-stable;
+    echo "Install R libraries";
+    Rscript setup.R;
+    source deactivate;
+fi
+if [ ! -d ~/.modules/qiime1 ]; then
+    echo "Create qiime1 environment";
+    conda create python=2.7 qiime matplotlib=1.4.3 mock nose -c bioconda --yes --quiet --copy -p ~/.modules/qiime1 &>/dev/null;
+fi
 
 # Configure and install environment modules
-echo "Install environment-modules";
-wget https://sourceforge.net/projects/modules/files/Modules/modules-4.2.1/modules-4.2.1.tar.gz -O modules-4.2.1.tar.gz &>/dev/null;
-tar -zxf modules-4.2.1.tar.gz;
-cd modules-4.2.1;
-./configure --prefix="${HOME}/.local" --modulefilesdir="${HOME}/.modules/modulefiles" #&>/dev/null;
-make;
-sudo make install;
-cd $REPO_DIR;
+if [ ! -d ~/.local ]; then
+    echo "Install environment-modules";
+    wget https://sourceforge.net/projects/modules/files/Modules/modules-4.2.1/modules-4.2.1.tar.gz -O modules-4.2.1.tar.gz &>/dev/null;
+    tar -zxf modules-4.2.1.tar.gz;
+    cd modules-4.2.1;
+    ./configure --prefix="${HOME}/.local" --modulefilesdir="${HOME}/.modules/modulefiles" &>/dev/null;
+    make &>/dev/null;
+    sudo make install &>/dev/null;
+    cd $REPO_DIR;
+fi
 
 # Make sure module will work
 export PATH="$HOME/.local/bin:$PATH"
 sudo ln -s "${HOME}/.local/init/profile.sh /etc/profile.d/modules.sh";
 sed -i "\$asource ${HOME}/.local/init/bash" ~/.bashrc;
 
-# Create links to the conda envs
+#if [ ! -d ~/.modules/qiime2 ]; then
+#    echo "Create qiime2 environment"
+#    wget https://data.qiime2.org/distro/core/qiime2-2019.1-py36-linux-conda.yml -O ~/qiime2.yml --quiet;
+#    conda env create --file ~/qiime2.yml --quiet -p ~/.modules/qiime2 &>/dev/null;
+#fi
+
+# Create links
 ln -sf ~/.modules/qiime1 ~/miniconda2/envs/qiime1;
 ln -sf ~/.modules/qiime2 ~/miniconda2/envs/qiime2;
 ln -sf ~/.modules/mmeds-stable ~/miniconda2/envs/mmeds-stable;
-
-# Create links to the module files
-# TEMPORARY
-rm -rf ~/.modules/modulefiles/qiime1
-rm -rf ~/.modules/modulefiles/qiime2
-mkdir ~/.modules/modulefiles/qiime
-mkdir ~/.modules/modulefiles/qiime2
-ln -sf $REPO_DIR/modules/mmeds-stable ~/.modules/modulefiles/mmeds-stable
-ln -sf $REPO_DIR/modules/qiime2 ~/.modules/modulefiles/qiime2/2019.1
-ln -sf $REPO_DIR/modules/qiime1 ~/.modules/modulefiles/qiime/1.9.1
 
 cd $REPO_DIR;
 echo "Finished setup. In $(pwd)"
