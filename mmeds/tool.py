e64b7601fea97a7f7da51888d670adc1c28e715a
dswallach <d.s.t.wallach@gmail.com>
2019-10-16 12:48:42 -0400

    queues are closed resetting

--- mmeds/tool.py
+++ mmeds/tool.py
@@ -423,7 +423,8 @@ class Tool(mp.Process):
                 # Set execute permissions
                 submitfile.chmod(0o770)
                 jobfile.chmod(0o770)
-                output = run([submitfile], check=True, capture_output=True)
+                #  Temporary for testing on Minerva
+                output = run([jobfile], check=True, capture_output=True)
                 job_id = int(str(output.stdout).split(' ')[1].strip('<>'))
                 self.wait_on_job(job_id)
             debug_log('{}: pre post analysis'.format(self.name))
