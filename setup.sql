0e32480b81666037c711370591f902d0ea99cb2d
dswallach <d.s.t.wallach@gmail.com>
2020-03-04 14:02:01 -0500

    Changed to use memory tables

--- setup.sql
+++ setup.sql
@@ -1,4 +1,4 @@
-source sql/tables.sql;
-source sql/functions.sql;
-source sql/users.sql;
-source sql/views.sql;
+source sql/test_sql/tables.sql;
+source sql/test_sql/functions.sql;
+source sql/test_sql/users.sql;
+source sql/test_sql/views.sql;
