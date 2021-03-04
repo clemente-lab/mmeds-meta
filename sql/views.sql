864636980e19c0f9f1675374fe549ac8223ce154
dswallach <d.s.t.wallach@gmail.com>
2021-03-04 08:57:53 -0500

    line breaks

--- sql/views.sql
+++ sql/views.sql
@@ -5,32 +5,32 @@
 
 -- Specimen View --
 DROP VIEW IF EXISTS `mmeds_data1`.`SpecimenView`;
-CREATE VIEW IF NOT EXISTS `mmeds_data1`.`SpecimenView` AS SELECT
-`idSpecimen`,
-`SpecimenID`,
-`Specimen`.`user_id`,
-`SpecimenCollectionDate`,
-`SpecimenInformation`,
-`SpecimenCollectionTime`,
-`SpecimenWeight`,
-`StudyName`
-FROM ( `Specimen` INNER JOIN
-    ( `Experiment` INNER JOIN
-        `Study` ON `Study_idStudy` = `idStudy` )
+CREATE VIEW IF NOT EXISTS `mmeds_data1`.`SpecimenView` AS SELECT\
+`idSpecimen`,\
+`SpecimenID`,\
+`Specimen`.`user_id`,\
+`SpecimenCollectionDate`,\
+`SpecimenInformation`,\
+`SpecimenCollectionTime`,\
+`SpecimenWeight`,\
+`StudyName`\
+FROM ( `Specimen` INNER JOIN\
+    ( `Experiment` INNER JOIN\
+        `Study` ON `Study_idStudy` = `idStudy` )\
     ON `Experiment_idExperiment` = `idExperiment` );
 
 -- Sample View --
 DROP VIEW IF EXISTS `mmeds_data1`.`SampleView`;
-CREATE VIEW IF NOT EXISTS `mmeds_data1`.`SampleView` AS SELECT
-`idSample`, `Aliquot_idAliquot`,
-`SampleDatePerformed`,
-`SampleProcessor`,
-`SampleProtocolInformation`,
-`SampleProtocolID`,
-`SampleConditions`,
-`SampleTool`,
-`SampleToolVersion`
-FROM ( `Sample` INNER JOIN
-    ( `SampleProtocol` INNER JOIN
-        `SampleProtocols` ON `SampleProtocols_idSampleProtocols` = `idSampleProtocols` )
+CREATE VIEW IF NOT EXISTS `mmeds_data1`.`SampleView` AS SELECT\
+`idSample`, `Aliquot_idAliquot`,\
+`SampleDatePerformed`,\
+`SampleProcessor`,\
+`SampleProtocolInformation`,\
+`SampleProtocolID`,\
+`SampleConditions`,\
+`SampleTool`,\
+`SampleToolVersion`\
+FROM ( `Sample` INNER JOIN\
+    ( `SampleProtocol` INNER JOIN\
+        `SampleProtocols` ON `SampleProtocols_idSampleProtocols` = `idSampleProtocols` )\
     ON `SampleProtocol_idSampleProtocol` = `idSampleProtocol` );
