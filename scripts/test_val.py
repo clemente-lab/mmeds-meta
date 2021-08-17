c94e35fe1092be24e2d508fc9bf0979ee652177a
dswallach <d.s.t.wallach@gmail.com>
2021-08-17 13:20:51 -0400

    preparing for merge

--- scripts/test_val.py
+++ scripts/test_val.py
@@ -1,15 +1,15 @@
 from mmeds.util import simplified_to_full
 import mmeds.validate as valid
 from pathlib import Path
+print('Checking Subject')
 errors, warnings, subjects = valid.validate_mapping_file('/home/david/Work/minerva/data/Lee_Tran/GRAPHS_Subject.tsv',
                                                          'GRAPHS',
                                                          'subject',
                                                          None,
                                                          'human')
 
-new_path = Path('/home/david/Work/minerva/data/Lee_Tran/GRAPHS_Specimen.tsv')
-simplified_to_full('/home/david/Work/minerva/data/Lee_Tran/Lee_Specimen_clean.tsv', new_path, 'specimen')
-errors, warnings, subjects = valid.validate_mapping_file(new_path,
+print('Checking Specimen')
+errors, warnings, subjects = valid.validate_mapping_file('/home/david/Work/minerva/data/Lee_Tran/GRAPHS_Specimen.tsv',
                                                          'GRAPHS',
                                                          'specimen',
                                                          subjects,
