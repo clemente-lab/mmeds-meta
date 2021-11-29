d4e8e58dc73f0a2ca952df8da2d0edfc9c87d2db
Adam Cantor <adamcantor22@gmail.com>
2021-11-29 12:45:08 -0500

    updates

--- mmeds/util.py
+++ mmeds/util.py
@@ -1308,32 +1308,64 @@ def join_taxa_with_correlations(taxonomy, correlation_files, out_dir, significan
         for i in range(len(corr_df['Feature ID'])):
             pval = corr_df['pval'][i]
             if pval < significance:
+                test_stat = corr_df['Test stat.'][i]
                 pval_fdr = corr_df['pval_fdr'][i]
                 pval_bon = corr_df['pval_bon'][i]
                 feature_id = corr_df['Feature ID'][i]
-                out_dict[f.name][feature_id] = (taxa_dict[feature_id], pval, pval_fdr, pval_bon)
+                out_dict[f.name][feature_id] = (feature_id, taxa_dict[feature_id], test_stat, pval, pval_fdr, pval_bon)
     out_files = []
     for d in out_dict:
-        variable = re.match(repr(fig.CORRELATION_FILENAME_TEMPLATE.format('(.+)')), d).group(0)
+        variable = re.match(r'(.+)_correlation\.tsv', d).group(1)
         out_file = out_dir / fig.CORRELATION_TAXA_FILENAME_TEMPLATE.format(variable)
-        df = pd.DataFrame.from_dict(d, orient='index')
-        df.columns = ['Feature ID', 'Taxon', 'pval', 'pval_fdr', 'pval_bon']
+        df = pd.DataFrame.from_dict(out_dict[d], orient='index')
+        df.columns = ['Feature ID', 'Taxon', 'Test stat.', 'pval', 'pval_fdr', 'pval_bon']
         df.to_csv(out_file, sep='\t')
         out_files.append(out_file)
 
+    print('filename')
+    print(len(out_files))
+    for f in out_files:
+        print(f.name)
     return out_files
 
 
-def generate_variable_abundance(feature_table, taxonomy, mapping_file, correlation_files):
-    feat_df = pd.read_csv(feature_table, sep='\t', skiprows=[0], header=[1])
+def generate_variable_abundance(feature_table, mapping_file, correlation_files):
+    feat_df = pd.read_csv(feature_table, sep='\t', skiprows=[0], header=[0])
     map_df = pd.read_csv(mapping_file, sep='\t', skiprows=[1], header=[0])
 
     corr_dfs = {}
     for f in correlation_files:
-        variable = re.match(repr(fig.CORRELATION_TAXA_FILENAME_TEMPLATE.format('(.+)')), f).group(0)
+        print(f.name)
+        variable = re.match(r'(.+)_correlation_taxa\.tsv', f.name).group(1)
         corr_dfs[variable] = pd.read_csv(f, sep='\t', header=[0])
 
+    feat_dict = {}
+    for key in feat_df:
+        if key == '#OTU_ID':
+            continue
+        for i in range(len(feat_df['#OTU ID'])):
+            feat_dict[(key, feat_df['#OTU ID'][i])] = feat_df[key][i]
+
+    map_dict = {}
+    for variable in corr_dfs:
+        for i in range(len(map_df['#SampleID'])):
+            map_dict[(map_df['#SampleID'][i], variable)] = map_df[variable][i]
+
     out_dict = {}
+    for (sample_id, variable) in map_dict:
+        for i in range(len(corr_dfs[variable]['Feature ID'])):
+            feature_id = corr_dfs[variable]['Feature ID'][i]
+            taxon = corr_dfs[variable]['Taxon'][i]
+            abundance = feat_dict[(sample_id, feature_id)]
+            concentration = map_dict[(sample_id, variable)]
+            if pd.isna(concentration):
+                concentration = 'NA'
+                print(concentration)
+            out_dict[(variable, sample_id, feature_id)] = [variable, sample_id, feature_id, taxon, abundance, concentration]
+
+    df = pd.DataFrame.from_dict(out_dict, orient='index')
+    df.columns = ['Variable', 'Sample ID', 'Feature ID', 'Taxon', 'Read Counts', 'Concentration']
+    return df
 
 
 
