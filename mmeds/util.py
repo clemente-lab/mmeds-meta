97e09b7d7feb29835ed49e0e06654f3148211924
mstapylton <mkstapylton@gmail.com>
2022-03-30 11:41:28 -0400

    working almost locally, try remotely

--- mmeds/util.py
+++ mmeds/util.py
@@ -1328,7 +1328,7 @@ def run_analysis(path, tool_type):
         # x = run(gunzip_forward_barcodes, env=new_env, capture_output=True, shell=True, timeout=120)
         print('ran')
         print(x)
-        if 'qiime: not found' in x:
+        if 'qiime: not found' in str(x):
             raise Exception
 
     except CalledProcessError as e:
