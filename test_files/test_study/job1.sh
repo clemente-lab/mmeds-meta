e7508a1980b5ebcbff7d80d1061ad1f8d4dca0f2
mstapylton <mkstapylton@gmail.com>
2022-04-06 19:43:15 -0400

    only 2 cores available

--- test_files/test_study/job1.sh
+++ test_files/test_study/job1.sh
@@ -34,7 +34,7 @@ qiime tools import --type EMPSingleEndSequences --input-path $RUN_Qiime2/import_
 echo "MMEDS_STAGE_1"
 qiime demux emp-single --i-seqs $RUN_Qiime2/qiime_artifact.qza --m-barcodes-file $RUN_Qiime2/qiime_mapping_file.tsv --m-barcodes-column BarcodeSequence --o-error-correction-details $RUN_Qiime2/error_correction.qza --o-per-sample-sequences $RUN_Qiime2/demux_file.qza;
 qiime demux summarize --i-data $RUN_Qiime2/demux_file.qza --o-visualization $RUN_Qiime2/demux_viz.qzv;
-qiime dada2 denoise-single --i-demultiplexed-seqs $RUN_Qiime2/demux_file.qza --p-trim-left 0 --p-trunc-len 0 --o-representative-sequences $RUN_Qiime2/rep_seqs_dada2.qza --o-table $RUN_Qiime2/table_dada2.qza --o-denoising-stats $RUN_Qiime2/stats_dada2.qza --p-n-threads 10;
+qiime dada2 denoise-single --i-demultiplexed-seqs $RUN_Qiime2/demux_file.qza --p-trim-left 0 --p-trunc-len 0 --o-representative-sequences $RUN_Qiime2/rep_seqs_dada2.qza --o-table $RUN_Qiime2/table_dada2.qza --o-denoising-stats $RUN_Qiime2/stats_dada2.qza --p-n-threads 1;
 qiime metadata tabulate --m-input-file $RUN_Qiime2/stats_dada2.qza --o-visualization $RUN_Qiime2/stats_dada2_visual.qzv;
 echo "MMEDS_STAGE_2"
 qiime feature-table filter-samples --i-table $RUN_Qiime2/table_dada2.qza --m-metadata-file $RUN_Qiime2/qiime_mapping_file.tsv --o-filtered-table $RUN_Qiime2/filtered_table.qza
@@ -43,7 +43,7 @@ qiime alignment mafft --i-sequences $RUN_Qiime2/rep_seqs_dada2.qza --o-alignment
 qiime alignment mask --i-alignment $RUN_Qiime2/alignment.qza --o-masked-alignment $RUN_Qiime2/masked_alignment.qza;
 qiime phylogeny fasttree --i-alignment $RUN_Qiime2/masked_alignment.qza --o-tree $RUN_Qiime2/unrooted_tree.qza;
 qiime phylogeny midpoint-root --i-tree $RUN_Qiime2/unrooted_tree.qza --o-rooted-tree $RUN_Qiime2/rooted_tree.qza;
-qiime diversity core-metrics-phylogenetic --i-phylogeny $RUN_Qiime2/rooted_tree.qza --i-table $RUN_Qiime2/filtered_table.qza --p-sampling-depth 1114 --m-metadata-file $RUN_Qiime2/qiime_mapping_file.tsv --p-n-jobs-or-threads 10  --output-dir $RUN_Qiime2/core_metrics_results;
+qiime diversity core-metrics-phylogenetic --i-phylogeny $RUN_Qiime2/rooted_tree.qza --i-table $RUN_Qiime2/filtered_table.qza --p-sampling-depth 1114 --m-metadata-file $RUN_Qiime2/qiime_mapping_file.tsv --p-n-jobs-or-threads 1  --output-dir $RUN_Qiime2/core_metrics_results;
 echo "MMEDS_STAGE_3"
 qiime diversity alpha-group-significance --i-alpha-diversity $RUN_Qiime2/core_metrics_results/faith_pd_vector.qza --m-metadata-file $RUN_Qiime2/qiime_mapping_file.tsv --o-visualization $RUN_Qiime2/faith_pd_group_significance.qzv&
 qiime diversity alpha-group-significance --i-alpha-diversity $RUN_Qiime2/core_metrics_results/shannon_vector.qza --m-metadata-file $RUN_Qiime2/qiime_mapping_file.tsv --o-visualization $RUN_Qiime2/shannon_group_significance.qzv&
@@ -54,7 +54,7 @@ qiime diversity beta-group-significance --i-distance-matrix $RUN_Qiime2/core_met
 qiime diversity alpha-rarefaction --i-table $RUN_Qiime2/filtered_table.qza --i-phylogeny $RUN_Qiime2/rooted_tree.qza --p-max-depth 4000 --m-metadata-file $RUN_Qiime2/qiime_mapping_file.tsv --o-visualization $RUN_Qiime2/alpha_rarefaction.qzv&
 wait
 echo "MMEDS_STAGE_4"
-qiime feature-classifier classify-sklearn --i-classifier $test_files/gg-13-8-99-nb-classifier.qza --i-reads $RUN_Qiime2/rep_seqs_dada2.qza --o-classification $RUN_Qiime2/taxonomy.qza --p-n-jobs 10
+qiime feature-classifier classify-sklearn --i-classifier $test_files/gg-13-8-99-nb-classifier.qza --i-reads $RUN_Qiime2/rep_seqs_dada2.qza --o-classification $RUN_Qiime2/taxonomy.qza --p-n-jobs 1
 qiime taxa barplot --i-table $RUN_Qiime2/filtered_table.qza --i-taxonomy $RUN_Qiime2/taxonomy.qza --m-metadata-file $RUN_Qiime2/qiime_mapping_file.tsv --o-visualization $RUN_Qiime2/taxa_bar_plot.qzv
 qiime composition add-pseudocount --i-table $RUN_Qiime2/filtered_table.qza --o-composition-table $RUN_Qiime2/comp-SpecimenBodySite-table.qza
 qiime composition ancom --i-table $RUN_Qiime2/comp-SpecimenBodySite-table.qza --m-metadata-file $RUN_Qiime2/qiime_mapping_file.tsv --p-transform-function log --m-metadata-column SpecimenBodySite --o-visualization $RUN_Qiime2/ancom-SpecimenBodySite.qzv
