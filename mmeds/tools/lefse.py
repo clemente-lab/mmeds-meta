429e2d39cdc9d91b70751adb52de9be89698623f
Adam Cantor <adamcantor22@gmail.com>
2022-10-12 10:43:33 -0400

    svg format

--- mmeds/tools/lefse.py
+++ mmeds/tools/lefse.py
@@ -63,7 +63,7 @@ class Lefse(Tool):
     def plot_results(self):
         """ Create basic plot of the results """
 
-        self.add_path('results_plot', '.png')
+        self.add_path('results_plot', '.svg')
         cmd = 'lefse_plot_res.py {input_file} {plot} --format svg --max_feature_len 160 subclades -1;'
         self.jobtext.append(cmd.format(input_file=self.get_file('lefse_results'),
                                        plot=self.get_file('results_plot')))
@@ -71,7 +71,7 @@ class Lefse(Tool):
     def cladogram(self):
         """ Create cladogram of the results """
 
-        self.add_path('results_cladogram', '.png')
+        self.add_path('results_cladogram', '.svg')
         cmd = 'lefse_plot_cladogram.py {input_file} {cladogram} --format svg;'
         self.jobtext.append(cmd.format(input_file=self.get_file('lefse_results'),
                                        cladogram=self.get_file('results_cladogram')))
