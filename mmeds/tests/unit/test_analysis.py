48fac270edc691a84657357af3520fd438196c57
Adam Cantor <adamcantor22@gmail.com>
2024-07-25 11:30:22 -0400

    testing analysis

--- mmeds/tests/unit/test_analysis.py
+++ mmeds/tests/unit/test_analysis.py
@@ -2,8 +2,11 @@ from unittest import TestCase
 from pathlib import Path
 import mmeds.config as fig
 
-from mmeds.util import run_analysis
+from mmeds.util import run_analysis, load_config
 from mmeds.summary import summarize_qiime
+from mmeds.spawn import Watcher
+from mmeds.tools.analysis import Analysis
+from mmeds.database.database import Database
 
 
 class AnalysisTests(TestCase):
@@ -14,13 +17,31 @@ class AnalysisTests(TestCase):
         # path to an example study, created from a MMEDs upload of existing test files
         # definied in config.py
         cls.test_study = fig.TEST_STUDY
+        cls.watcher = Watcher()
+        cls.watcher.connect()
+        cls.queue = cls.watcher.get_queue()
+        with Database(owner=fig.TEST_USER, testing=True) as db:
+            cls.analysis_code = db.create_access_code()
+        cls.config = load_config(fig.DEFAULT_CONFIG, fig.TEST_METADATA_SINGLE, 'standard_pipeline')
 
-    def test_standard_pipeline(self):
+    def test_a_standard_pipeline(self):
         """ Test running a standard analysis """
         # run_analysis() executes analyses synchronously rather than submitting as a job
         run_analysis(self.test_study, 'standard_pipeline', testing=True)
-        #summarize_qiime(f'{self.test_study}/Qiime2_0', 'standard_pipeline', testing=True)
+        """
+        summarize_qiime(f'{self.test_study}/Qiime2_0', 'standard_pipeline', testing=True)
 
-        # check for summary pdf, also gets uploaded as an artifact in github actions for inspection
-        #pdf_output = Path(f'{self.test_study}/Qiime2_0/summary/mkstapylton@gmail.com-mattS-qiime2.ipynb')
-        #self.assertTrue(pdf_output.exists())
+         check for summary pdf, also gets uploaded as an artifact in github actions for inspection
+        pdf_output = Path(f'{self.test_study}/Qiime2_0/summary/mkstapylton@gmail.com-mattS-qiime2.ipynb')
+        self.assertTrue(pdf_output.exists())
+        """
+
+    def test_b_analysis_class(self):
+        """ Test the analysis object """
+        analysis = Analysis(self.queue, fig.TEST_USER, self.analysis_code, fig.TEST_CODE_SHORT, 'standard_pipeline', 'default', 'test_init', self.config, True, {}, False)
+        analysis.run()
+
+        info = analysis.get_info()
+        self.assertEquals(info['owner'], fig.TEST_USER)
+        self.assertEquals(info['analysis_code'], self.analysis_code)
+
