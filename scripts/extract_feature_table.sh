06f04aea56740fd6b194e25be4b1f7f10a8be8bf
Kevin Bu <kbpi314@gmail.com>
2025-06-11 11:01:42 -0400

    fixed var syntax

--- scripts/extract_feature_table.sh
+++ scripts/extract_feature_table.sh
@@ -8,14 +8,14 @@ file_type="${3:-tsv}" # tsv, biom, or fasta
 tmp_dir_name="${4:-tmp_unzip}"
 unzip -joq $in_file -d $tmp_dir_name
 
-if [["$file_type" = "tsv"]]; then
+if [["$file_type" == "tsv"]]; then
     biom convert --to-tsv -i $tmp_dir_name/feature-table.biom -o $out_file
     sed -i '1d;2s/^#//' $out_file
     rm -rf $tmp_dir_name
-elif [["$file_type" = "biom"]]; then
+elif [["$file_type" == "biom"]]; then
     cp $tmp_dir_name/feature-table.biom $out_file
     rm -rf $tmp_dir_name
-elif [["$file_type" = "fasta"]]; then
+elif [["$file_type" == "fasta"]]; then
     cp $tmp_dir_name/dna-sequences.fasta $out_file
     rm -rf $tmp_dir_name
 else
